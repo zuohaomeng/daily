ccc24f82cad174d6a468e7ef4755eb6719b1c45b
zuohaomeng <178183852@qq.com>
2019-11-23 18:15:37 +0800

    Initial commit

---
+++ rocketmq/src/main/java/com/meng/daily/rocketmq/RocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.meng.daily.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meng/daily/spb/SpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.daily.spb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
