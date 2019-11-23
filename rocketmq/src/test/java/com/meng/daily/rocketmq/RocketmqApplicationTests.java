eb6114ca616f0ab35ef2c478e0e03c53ce295a73
zuohaomeng <178183852@qq.com>
2019-11-23 18:30:50 +0800

    2

---
+++ rocketmq/src/test/java/com/meng/daily/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.daily.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
