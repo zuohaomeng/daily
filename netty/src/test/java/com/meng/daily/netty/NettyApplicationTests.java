b7b552769fd4e248181dc858849ccab1ec0379f6
zuohaomeng <178183852@qq.com>
2019-12-28 16:29:03 +0800

    netty

---
+++ netty/src/test/java/com/meng/daily/netty/NettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.daily.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
