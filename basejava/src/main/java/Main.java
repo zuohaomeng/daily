5417257db42adffba315781d01df7d2a31ba57a7
zuohaomeng <178183852@qq.com>
2020-03-12 21:55:31 +0800

    Main

--- basejava/src/main/java/Main.java
+++ basejava/src/main/java/Main.java
@@ -1,40 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        if(n == 1){
-            System.out.println(-1);
-            return;
-        }
 
-        num[1][1] = 1;
-        num[1][2] = 0;
-        for (int i = 2; i <= n; i++) {
-            if (s1.charAt(i - 1) == '.') {
-                num[1][i] = num[1][i - 1] + num[2][i - 1];
-            } else {
-                num[1][i] = 0;
-            }
-            if (s2.charAt(i - 1) == '.') {
-                num[2][i] = num[2][i - 1] + num[1][i - 1];
-            } else {
-                num[2][i] = 0;
-            }
-            if (num[1][i] == 0 && num[2][i] == 0) {
-                System.out.println(-1);
-                return;
-            }
-        }
-        System.out.println(num[2][n]);
     }
 }
-/*
-1
-.
-.
- */
