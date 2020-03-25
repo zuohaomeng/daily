8f1e1b2658c1e8bcfd45fc3f84a02018cbd4c8bd
zuohaomeng <178183852@qq.com>
2020-03-25 19:09:19 +0800

    Main

---
+++ smallspring/src/main/java/com/meng/smallspring/SmallspringApplication.java
@@ -0,0 +1,13 @@
+package com.meng.smallspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallspringApplication.class, args);
+    }
+
+}
---
+++ smallspring/src/test/java/com/meng/smallspring/SmallspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.smallspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
