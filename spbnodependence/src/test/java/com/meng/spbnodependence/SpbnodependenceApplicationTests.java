427727585ecf5c02b165ba42a7762cb74961069a
左浩 <hao.zuo2@zhenai.com>
2020-08-04 16:39:17 +0800

    8

---
+++ spbnodependence/src/test/java/com/meng/spbnodependence/SpbnodependenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.spbnodependence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbnodependenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
